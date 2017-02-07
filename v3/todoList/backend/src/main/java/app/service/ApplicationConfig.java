f4ef5a862cfda6bb1fcc2c1391ae84ecb28989bd
PauloGladson <paulo.pinheiro@serpro.gov.br>
2017-02-07 11:33:27 -0200

    Backend v3

--- v3/todoList/backend/src/main/java/app/service/ApplicationConfig.java
+++ v3/todoList/backend/src/main/java/app/service/ApplicationConfig.java
@@ -19,6 +19,13 @@ public class ApplicationConfig extends Application {
         BeanConfig beanConfig = new BeanConfig();
         beanConfig.setVersion("1.0.0");
         beanConfig.setBasePath("/api");
+        beanConfig.setContact("https://github.com/demoiselle");
+        beanConfig.setDescription("Sistema de exemplo do Demoiselle v3 - ToDolist https://github.com/demoiselle/example ");
+        beanConfig.setHost("demoiselle.io");
+        beanConfig.setLicense("LGPL v2");
+        beanConfig.setVersion("3.0.0");
+        beanConfig.setTermsOfServiceUrl("https://demoiselle.gitbooks.io/documentacao-jee/");
+        beanConfig.setTitle("Demoiselle ToDoList - API");
         beanConfig.setResourcePackage("app.service");
         beanConfig.setScan(true);
     }
