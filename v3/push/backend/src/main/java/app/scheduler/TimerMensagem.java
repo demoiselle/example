5a55d39e6c6d8ed0b5363bb78bb958bfb46c8f5e
PauloGladson <paulo.pinheiro@serpro.gov.br>
2017-03-10 12:13:36 -0300

    Ajustes no canal

--- v3/push/backend/src/main/java/app/scheduler/TimerMensagem.java
+++ v3/push/backend/src/main/java/app/scheduler/TimerMensagem.java
@@ -10,26 +10,26 @@ import app.socket.push.PushMessage;
 import com.google.gson.Gson;
 import java.util.Date;
 import javax.ejb.Schedule;
-import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
+import javax.inject.Singleton;
 
 /**
  *
  * @author 70744416353
  */
-@ApplicationScoped
+@Singleton
 public class TimerMensagem {
 
     @Inject
     private PushEndpoint pe;
 
-    @Schedule(minute = "*/3", second = "33", persistent = false)
+    @Schedule(second = "30", minute = "*/3", hour = "*", persistent = false)
     public void sendHello() {
         PushMessage mm = new PushMessage("echo", "Sistema de Push Demoiselle, https://github.com/demoiselle/example/tree/master/v3/push");
         pe.sendTo(new Gson().toJson(mm), "echo");
     }
 
-    @Schedule(minute = "*/1", second = "00", persistent = false)
+    @Schedule(second = "00", minute = "*/1", hour = "*", persistent = false)
     public void sendHora() {
         PushMessage mm = new PushMessage("time", (new Date(System.nanoTime())).toString());
         pe.sendTo(new Gson().toJson(mm), "time");
