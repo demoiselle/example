715136ddfb673e27b00fe992e51c17d51ce3aa53
PauloGladson <paulo.pinheiro@serpro.gov.br>
2017-04-19 08:03:20 -0300

    Ajuste para uso de banco NoSql

--- v3/cep/backend/src/main/java/app/dao/CepDAO.java
+++ v3/cep/backend/src/main/java/app/dao/CepDAO.java
@@ -21,21 +21,23 @@ public class CepDAO extends AbstractDAO<Cep, String> {
     }
 
     public Object getCep(String cep) {
-        return em.createQuery("From Cep c Where c.cep = :cep").setParameter("cep", cep).getResultList();
+        return em.createQuery("From Cep c Where c.cep = :cep", Cep.class).setParameter("cep", cep).getResultList();
     }
 
     public Object getCidades(String uf) {
-        return em.createNativeQuery("db.cep.aggregate([{$match:{uf:\"" + uf + "\"}}, {$group:{_id:\"$cidade\"}}]);").getResultList();
-    }
-
-    public Object getLogradouro(String uf, String nome) {
-        return em.createNativeQuery("db.cep.find({\"uf\":\"" + uf + "\"})").getResultList();
-    }
-
-    public Object getUfs() {
-        String query1 = "db.cep.aggregate({$group : {_id : \"$uf\"})";
-        //,{$sort : {total : -1}}
+        String query1 = "db.cep.aggregate([{ '$match': { 'uf': '" + uf + "'  } },  { '$group' : { '_id': '$cidade' } }, { '$sort': { '_id': 1 } } ])";
         Query query = em.createNativeQuery(query1);
         return query.getResultList();
     }
+
+    public Object getLogradouro(String uf, String nome) {
+        return em.createQuery("From Cep c Where c.uf = :uf and c.logradouro like :nome", Cep.class).setParameter("uf", uf).setParameter("nome", "%" + nome).getResultList();
+    }
+
+    public Object getUfs() {
+        String query1 = "db.cep.aggregate([ { '$group' : { '_id' : '$uf' } }, { '$sort' : { '_id' : 1 } } ])";
+        Query query = em.createNativeQuery(query1);
+        return query.getResultList();
+    }
+
 }
