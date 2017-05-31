d92d75129a275918a65c4d62d2f76fc5128387e6
PauloGladson <paulopinheiro777@gmail.com>
2017-05-31 11:50:31 -0300

    Foi

---
+++ v3/cep/backend/src/main/java/app/bc/CepBC.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package app.bc;
+
+import app.dao.CepDAO;
+import app.entity.Cep;
+import java.util.List;
+import org.demoiselle.jee.crud.AbstractBusiness;
+
+/**
+ *
+ * @author 70744416353
+ */
+public class CepBC extends AbstractBusiness<Cep, Integer> {
+
+    public List getEndereco(String id) {
+        return (((CepDAO) dao).getEndereco(id));
+    }
+}
