3a5795cd73292ebe751a07e9cf84c350f8811a5c
PauloGladson <paulopinheiro777@gmail.com>
2017-06-06 12:07:22 -0300

    Ajustes

--- v3/livraria/sql/database.sql
+++ v3/livraria/sql/database.sql
@@ -1,1 +1,3 @@
-CREATE TABLE livro (id uuid NOT NULL, descricao character varying(255) NOT NULL, CONSTRAINT livro_pkey PRIMARY KEY (id));
+CREATE TABLE livro (id uuid NOT NULL, descricao character varying(255) NOT NULL, CONSTRAINT livro_pkey PRIMARY KEY (id),CONSTRAINT uk_livro UNIQUE(descricao));
+
+
