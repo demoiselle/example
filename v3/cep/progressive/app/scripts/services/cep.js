9c641f16432251fe7aa9498429f286a9119a1f08
Paulo Gladson Ximenes Pinheiro <paulo.pinheiro@serpro.gov.br>
2017-07-31 17:21:41 -0300

    Busca com like

--- v3/cep/progressive/app/scripts/services/cep.js
+++ v3/cep/progressive/app/scripts/services/cep.js
@@ -4,25 +4,25 @@ app.factory('CepService', ['$http', function ($http) {
         var service = {};
 
         service.getMunicipios = function (uf) {
-            return $http.get('api/v1/cidades/' + uf).then(function (res) {
+            return $http.get('https://cep-fwkdemoiselle.rhcloud.com/api/v1/cidades/' + uf).then(function (res) {
                 return res;
             });
         };
 
         service.getLogradouro = function (uf, logra) {
-            return $http.get('api/v1/logradouro/' + uf + '/' + logra).then(function (res) {
+            return $http.get('https://cep-fwkdemoiselle.rhcloud.com/api/v1/logradouro/' + uf + '/' + logra).then(function (res) {
                 return res;
             });
         };
 
         service.getUf = function () {
-            return $http.get('api/v1/ufs').then(function (res) {
+            return $http.get('https://cep-fwkdemoiselle.rhcloud.com/api/v1/ufs').then(function (res) {
                 return res;
             });
         };
 
         service.getCep = function (cep) {
-            return $http.get('api/v1/ceps?cep=' + cep).then(function (res) {
+            return $http.get('https://cep-fwkdemoiselle.rhcloud.com/api/v1/ceps?cep=' + cep).then(function (res) {
                 return res;
             });
         };
