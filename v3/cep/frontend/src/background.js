4e3bcc4f95786cc73439642e75061ca5cc301e07
paulo <paulo@DESKTOP-D1P292A>
2017-11-08 01:29:45 +0000

    Ajustes nos templates

---
+++ v3/cep/frontend/src/background.js
@@ -0,0 +1,20 @@
+// React when a browser action's icon is clicked.
+chrome.browserAction.onClicked.addListener(function (tab) {
+    var viewTabUrl = chrome.extension.getURL('https://www.demoiselle.org/cep/index.html');
+    var imageUrl = 'https://www.demoiselle.org/images/logo-large.png';
+
+    // Look through all the pages in this extension to find one we can use.
+    var views = chrome.extension.getViews();
+    for (var i = 0; i < views.length; i++) {
+        var view = views[i];
+
+        // If this view has the right URL and hasn't been used yet...
+        if (view.location.href == viewTabUrl && !view.imageAlreadySet) {
+
+            // ...call one of its functions and set a property.
+            view.setImageUrl(imageUrl);
+            view.imageAlreadySet = true;
+            break; // we're done
+        }
+    }
+});
